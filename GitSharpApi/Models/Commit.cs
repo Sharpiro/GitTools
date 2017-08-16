3fde19751558d4cb362af88057daea175753c89b
sharpiro <dsharpbb09@gmail.com>
2017-08-16 12:47:31 -0400

    Add project files.

---
+++ GitSharpApi/Models/Commit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitSharpApi.Models
+{
+    public class Commit
+    {
+        public string Hash { get; set; }
+        public string Author { get; set; }
+        public DateTime AuthorDate { get; set; }
+        public string Message { get; set; }
+    }
+}
