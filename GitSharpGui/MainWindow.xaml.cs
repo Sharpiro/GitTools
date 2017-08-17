c34b8d55edd3b31c0220a15136f09def655eabd5
sharpiro <dsharpbb09@gmail.com>
2017-08-16 21:45:35 -0400

    showing git history and hash lookups

---
+++ GitSharpGui/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using GitSharpApi;
+using GitSharpGui.Tools;
+using GitSharpGui.ViewModels;
+using Microsoft.Win32;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace GitSharpGui
+{
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _viewModel = new MainWindowViewModel();
+        private ILogger _logger = LogManager.CreateNullLogger();
+        private MessageBoxFacade _messageBox;
+        private GitProcess _gitProcess = new GitProcess();
+        private GitApi _gitApi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+            _messageBox = new MessageBoxFacade(_logger);
+            _gitApi = new GitApi(_gitProcess);
+        }
+
+        private async void RepoTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                //using (var folderDialog = new FolderBrowserDialog())
+                //{
+                //    var dialogResult = folderDialog.ShowDialog();
+                //    if (dialogResult != System.Windows.Forms.DialogResult.OK)
+                //        throw new Exception("oop");
+                //    _viewModel.RepoPath = folderDialog.SelectedPath;
+                //}
+
+                //_viewModel.RepoPath = folderDialog.SelectedPath;
+                if (!Directory.Exists(_viewModel.RepoPath))
+                {
+                    _viewModel.Commits = new List<GitSharpApi.Models.Commit>();
+                    return;
+                }
+                //_messageBox.ShowInfo($"Repo Path: '{_viewModel.RepoPath}'");
+                _gitProcess.WorkingDirectory = _viewModel.RepoPath;
+                _viewModel.Commits = (await _gitApi.GetCommitsAsync()).ToList();
+            }
+            catch (Exception ex)
+            {
+                _messageBox.ShowError(ex);
+            }
+        }
+
+        private async void HashTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(_viewModel.CurrentHash))
+                {
+                    _viewModel.OutputText = null;
+                    return;
+                }
+
+                _viewModel.OutputText = await _gitApi.CatFileAsync(_viewModel.CurrentHash);
+            }
+            catch (Exception ex)
+            {
+                _messageBox.ShowError(ex);
+            }
+        }
+
+        private void DataGrid_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var grid = sender as System.Windows.Controls.DataGrid;
+                var x = grid.CurrentItem as DataRowView;
+                if (x == null)
+                {
+                    //_viewModel.CurrentHash = null;
+                    return;
+                }
+                _viewModel.CurrentHash = x["Hash"] as string;
+            }
+            catch (Exception ex)
+            {
+                _messageBox.ShowError(ex);
+            }
+        }
+    }
+}
---
+++ GitSharpGui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GitSharpGui.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+    }
+}
